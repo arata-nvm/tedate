ff9e498b4a862e9cd026f58c2e346d33ede307d4
arata <arata04962@gmail.com>
2020-08-10 15:32:34 +0900

    :sparkles: implement evaluation of expressions

--- main.ml
+++ main.ml
@@ -4,3 +4,12 @@ type exp =
   | Minus of exp * exp
   | Times of exp * exp
   | Divide of exp * exp
+
+let rec eval1 e =
+  match e with
+  | IntLit(n) -> n
+  | Plus(e1, e2) -> (eval1 e1) + (eval1 e2)
+  | Minus(e1, e2) -> (eval1 e1) - (eval1 e2)
+  | Times(e1, e2) -> (eval1 e1) * (eval1 e2)
+  | Divide(e1, e2) -> (eval1 e1) / (eval1 e2)
+  | _ -> failwith "unknown expression"
