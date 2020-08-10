f9bcda53b7b2f856ade54ffb5edad6ab526537ee
arata <arata04962@gmail.com>
2020-08-10 15:44:47 +0900

    :sparkles: add if and equal expressions

--- main.ml
+++ main.ml
@@ -5,6 +5,8 @@ type exp =
   | Minus of exp * exp
   | Times of exp * exp
   | Divide of exp * exp
+  | If of exp * exp * exp
+  | Eq of exp * exp
 
 type value =
   | IntVal of int
@@ -34,5 +36,19 @@ let rec eval2 e =
       match (eval2 e1, eval2 e2) with
       | (IntVal(n1), IntVal(n2)) -> IntVal(n1 * n2)
       | _ -> failwith "integer values expected"
+    end
+  | If(e1, e2, e3) ->
+    begin
+      match (eval2 e1) with
+      | BoolVal(true) -> eval2 e2
+      | BoolVal(false) -> eval2 e3
+      | _ -> failwith "wrong value"
+    end
+  | Eq(e1, e2) ->
+    begin
+      match (eval2 e1, eval2 e2) with
+      | (IntVal(n1), IntVal(n2)) -> BoolVal(n1 = n2)
+      | (BoolVal(b1), BoolVal(b2)) -> BoolVal(b1 = b2)
+      | _ -> failwith "wrong value"
     end
   | _ -> failwith "unknown expression e"
