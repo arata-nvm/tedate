a941ac7f927631b6dc8a4768def5eb09e7c89fa6
arata <arata04962@gmail.com>
2020-08-10 15:40:55 +0900

    :sparkles: add bool value

--- main.ml
+++ main.ml
@@ -1,9 +1,14 @@
 type exp =
   | IntLit of int
+  | BoolLit of bool
   | Plus of exp * exp
   | Minus of exp * exp
   | Times of exp * exp
   | Divide of exp * exp
+
+type value =
+  | IntVal of int
+  | BoolVal of bool
 
 let rec eval1 e =
   match e with
@@ -13,3 +18,21 @@ let rec eval1 e =
   | Times(e1, e2) -> (eval1 e1) * (eval1 e2)
   | Divide(e1, e2) -> (eval1 e1) / (eval1 e2)
   | _ -> failwith "unknown expression"
+
+let rec eval2 e =
+  match e with
+  | IntLit(n) -> IntVal(n)
+  | BoolLit(b) -> BoolVal(b)
+  | Plus(e1, e2) ->
+    begin
+      match (eval2 e1, eval2 e2) with
+      | (IntVal(n1), IntVal(n2)) -> IntVal(n1 + n2)
+      | _ -> failwith "integer values expected"
+    end
+  | Times(e1, e2) ->
+    begin
+      match (eval2 e1, eval2 e2) with
+      | (IntVal(n1), IntVal(n2)) -> IntVal(n1 * n2)
+      | _ -> failwith "integer values expected"
+    end
+  | _ -> failwith "unknown expression e"
