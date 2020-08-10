6aaa0ebf59e067b993ce163fa5f173a352e3b732
arata <arata04962@gmail.com>
2020-08-10 16:12:42 +0900

    :recycle: delete eval1

--- main.ml
+++ main.ml
@@ -13,18 +13,9 @@ type value =
   | IntVal of int
   | BoolVal of bool
 
-let rec eval1 e =
-  match e with
-  | IntLit(n) -> n
-  | Plus(e1, e2) -> (eval1 e1) + (eval1 e2)
-  | Minus(e1, e2) -> (eval1 e1) - (eval1 e2)
-  | Times(e1, e2) -> (eval1 e1) * (eval1 e2)
-  | Divide(e1, e2) -> (eval1 e1) / (eval1 e2)
-  | _ -> failwith "unknown expression"
-
-let rec eval2 e =
+let rec eval e =
   let binop f e1 e2 =
-    match (eval2 e1, eval2 e2) with
+    match (eval e1, eval e2) with
     | (IntVal(n1), IntVal(n2)) -> IntVal(f n1 n2)
     | _ -> failwith "integer values exprected"
   in
@@ -37,21 +28,21 @@ let rec eval2 e =
   | Divide(e1, e2) -> binop (/) e1 e2
   | If(e1, e2, e3) ->
     begin
-      match (eval2 e1) with
-      | BoolVal(true) -> eval2 e2
-      | BoolVal(false) -> eval2 e3
+      match (eval e1) with
+      | BoolVal(true) -> eval e2
+      | BoolVal(false) -> eval e3
       | _ -> failwith "wrong value"
     end
   | Eq(e1, e2) ->
     begin
-      match (eval2 e1, eval2 e2) with
+      match (eval e1, eval e2) with
       | (IntVal(n1), IntVal(n2)) -> BoolVal(n1 = n2)
       | (BoolVal(b1), BoolVal(b2)) -> BoolVal(b1 = b2)
       | _ -> failwith "wrong value"
     end
   | Greater(e1, e2) ->
     begin
-      match (eval2 e1, eval2 e2) with
+      match (eval e1, eval e2) with
       | (IntVal(n1), IntVal(n2)) -> BoolVal(n1 > n2)
       | _ -> failwith "wrong value"
     end
