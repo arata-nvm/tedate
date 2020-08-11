2bc48c519f0c1e5a1b1d60829e3dd092bb1a4d61
arata <arata04962@gmail.com>
2020-08-11 16:43:43 +0900

    :sparkles: implement simple type checking

---
+++ type.ml
@@ -0,0 +1,24 @@
+open Syntax;;
+
+type ty =
+  | TInt
+  | TBool
+
+let rec tcheck e =
+  match e with
+  | IntLit(_) -> TInt
+  | BoolLit(_) -> TBool
+  | Plus(e1, e2) ->
+    begin
+      match (tcheck e1, tcheck e2) with
+      | (TInt, TInt) -> TInt
+      | _ -> failwith "type error in Plus"
+    end
+  | If(e1, e2, e3) ->
+    begin
+      match (tcheck e1, tcheck e2, tcheck e3) with
+      | (TBool, TInt, TInt) -> TInt
+      | (TBool, TBool, TBool) -> TBool
+      | _ -> failwith "type error in IF"
+    end
+  | _ -> failwith "unknown expression"
